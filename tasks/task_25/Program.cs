72eaf68be7eb07c741e48a49ef918d5e9eca46b7
NeHaker0110 <108989377+NeHaker0110@users.noreply.github.com>
2022-10-19 22:41:08 +0300

    modified task 25

--- tasks/task_25/Program.cs
+++ tasks/task_25/Program.cs
@@ -5,12 +5,12 @@
 //2, 4 -> 16
 
 Console.WriteLine("Введите числа:");
-int numberA = int.Parse(Console.ReadLine());// Преобразует строковое значение в целочисленное значение
-int numberB = int.Parse(Console.ReadLine());// Преобразует строковое значение в целочисленное значение
+int numberA = int.Parse(Console.ReadLine() ?? "");// Преобразует строковое значение в целочисленное значение
+int numberB = int.Parse(Console.ReadLine() ?? "");// Преобразует строковое значение в целочисленное значение
 
 
 {
-   Console.Write("Натуральная степень числа А:");
-   Console.Write(Math.Pow(numberA,numberB));
+    Console.Write("Натуральная степень числа А:");
+    Console.Write(Math.Pow(numberA, numberB));
 }
 
