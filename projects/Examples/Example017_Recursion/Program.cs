c2d066effcbe1da71e13fcac6444a2cbdb5a5ca5
NeHaker0110 <108989377+NeHaker0110@users.noreply.github.com>
2022-10-19 21:56:56 +0300

    deleted gitignore

--- projects/Examples/Example017_Recursion/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿string path = "C:\Users\tula7\OneDrive\Рабочий стол\Examples\Example017_Recursion";
-DirectoryInfo di = new DirectoryInfo(path);
-Console.WriteLine(di.CreationTime);
-FileInfo[] fi = di.GetFiles();
-
-for(int i =0; i< fi.Length;i++)
-{
-    Console.WriteLine(fi[i].Name);
-}
