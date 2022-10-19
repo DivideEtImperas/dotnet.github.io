6fb3e7acf226d67b9e48fd0df2598a73cc62fba0
NeHaker0110 <108989377+NeHaker0110@users.noreply.github.com>
2022-10-19 22:44:59 +0300

    modified task 47

--- tasks/task_47/Program.cs
+++ tasks/task_47/Program.cs
@@ -13,21 +13,21 @@ m = 3, n = 4.
 int GetNumber(string message)
 {
     Console.WriteLine(message);
-    int number = int.Parse(Console.ReadLine());
+    int number = int.Parse(Console.ReadLine() ?? "");
     return number;
 }
 
 double[,] InitMatrix(int m, int n)
 {
     double[,] matrix = new double[m, n];
-  
+
     Random rnd = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.NextDouble()*10 ;
+            matrix[i, j] = rnd.NextDouble() * 10;
         }
     }
 
