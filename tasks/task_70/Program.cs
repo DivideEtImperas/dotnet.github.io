5dc8a6740ccee7f4c5e230a9c7de2fdb5fc59d90
Eduard <tula71ya@gmail.com>
2022-11-02 00:23:55 +0300

    add tasks

---
+++ tasks/task_70/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N.
+//M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//M = 4; N = 8 -> "4, 6, 7, 8"
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+void RecursionNum(int m,int n)
+{
+
+    if (m > n)
+        return;
+
+    RecursionNum(m,n - 1);
+    Console.Write(n + ", ");
+
+}
+int m = GetNumber("Введите число m");
+int n = GetNumber("Введите число n");
+
+RecursionNum(m,n);
---
+++ tasks/task_71/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+int RecursionNum(int n, int sum)
+{
+
+    if (n == 0)
+        return sum;
+    sum += n % 10;
+    return RecursionNum(n / 10, sum);
+
+}
+int n = GetNumber("Введите число:");
+int sum = 0;
+Console.WriteLine(RecursionNum(n, sum));
