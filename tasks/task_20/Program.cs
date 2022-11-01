1ce3804d4034aa934e058967a6477d8d7e82042f
Eduard <tula71ya@gmail.com>
2022-11-01 23:53:23 +0300

    add task 20

---
+++ tasks/task_20/Program.cs
@@ -0,0 +1,45 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает 
+//диапазон возможных координат точек в этой четверти (x и y).
+
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+string QuaterTest(int quater,int coord)
+{
+    if (quater==1)
+    {
+        return "x>0y>0";
+    }
+    if (quater==2)
+    {
+        return "x>0y>0";
+    }
+     if (quater==3)
+    {
+        return "x>0y>0";
+    }
+    if (quater==4)
+    {
+        return "x>0y>0";
+    }
+    return "такой четверти нет";
+}
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+
+int coordX = ReadData("Введите координату X не равную 0: ");
+int coordY = ReadData("Введите координату Y не равную 0: ");
+
+string quaterAnswer = QuaterTest(coordX, coordY);
+
+PrintData("Номер четверти: ", quaterAnswer.ToString());
