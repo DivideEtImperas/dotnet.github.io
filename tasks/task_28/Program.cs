b2321bce4efb451869280c6b0a02fc63411196f9
Eduard <tula71ya@gmail.com>
2022-11-02 00:03:25 +0300

    add tasks

---
+++ tasks/task_28/Program.cs
@@ -0,0 +1,20 @@
+﻿//Код для защиты от дурака
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool correctInput = false;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        correctInput = int.TryParse(Console.ReadLine(), out result);
+
+        if(!correctInput)
+        Console.WriteLine("Вы ввели не корректное число");
+        Console.WriteLine();
+    }
+    return result;
+}
+int temp = GetNumber("Введите число");
---
+++ tasks/task_33/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+int [] mas = new int[8];
+Random rnd = new Random();
+for (int i=0; i<8; i++)
+{
+    mas[i] = rnd.Next(0, 2);
+    Console.Write(" "+mas[i]);
+}
+
+
+/*
+Console.WriteLine("Введите размерность массива:");
+int demension = int.Parse(Console.ReadLine());
+Random rnd = new Random();
+int[] arr = new int[demension];
+
+for (int i = 0; i < demension; i++)
+{
+    arr[i] = rnd.Next(0,2);
+}
+
+var result = string.Join(", ", arr);
+Console.WriteLine(result);
+
+*/
