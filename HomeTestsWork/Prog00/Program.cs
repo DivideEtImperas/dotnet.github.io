a3fa72d9013f4543d3212ab8f2471824256704c4
Golishev Eduard Vladimirovich <tula71ya@gmail.com>
2023-01-28 23:21:15 +0300

    add folder prog00

---
+++ HomeTestsWork/Prog00/Program.cs
@@ -0,0 +1,54 @@
+﻿static int[] CreateArray()
+{
+  return new int[10];
+}
+void Fill(int[]elements)
+
+{
+int length = elements.Length;
+int index = 0;
+
+while (index < length)
+{
+  int current = Random.Shared.Next(30);
+  elements[index] = current;
+  
+  index = index + 1;
+}
+}
+
+int GetSum(int[] array)
+{
+  int size = array.Length;
+  int sum = 0;
+
+  int position = 0;
+
+  while(position < size)
+  {
+    int current = array[position];
+    if(current >= 10 && current <= 99)
+    {
+      sum = sum + current;
+    }
+  }
+  return sum;
+}
+
+string Print(int[] collection)
+{
+  string output = String.Empty;
+
+  int size = collection.Length;
+  int index = 0;
+  while(index < size)
+  {
+    int current = collection[index];
+    output = output + $"{current}";
+  }
+  return output;
+}
+
+int[] arr = CreateArray();
+Fill(arr);    
+
